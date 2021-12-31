265594daea44defc2b1ecd358064cf54951d952d
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-12-31 22:05:22 +0545

    optimization

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://startup-agency-modern.vercel.app',
+  siteUrl: 'sushantadhikari.netlify.app',
   generateRobotsTxt: true,
   // optional
   // robotsTxtOptions: {
